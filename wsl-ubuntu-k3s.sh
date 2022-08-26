07e67048519b5076d0ac3b5131498088b8bd9881
sskk <susukkekki@gmail.com>
2022-08-26 15:36:39 +0900

    echo a comment

--- wsl-ubuntu-k3s.sh
+++ wsl-ubuntu-k3s.sh
@@ -5,8 +5,8 @@ curl https://get.docker.com | sh
 # sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo service docker start
-echo "You need to reopen a wsl shell to run 'docker ps' without sudo."
 
 # Install k3s
 # ! This requires systemd installation in advance.
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.18.20+k3s1 sh -s - --write-kubeconfig-mode 644 --no-deploy traefik --docker
+echo "You need to reopen a wsl shell to run 'docker ps' without sudo."
