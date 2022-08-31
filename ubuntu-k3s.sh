bd83564936201dcc0e7c93a7fdf4a3f101574980
sskk <susukkekki@gmail.com>
2022-09-01 00:45:39 +0900

    v1.18.20+k3s1 -> v1.24.4+k3s1 sh

--- ubuntu-k3s.sh
+++ ubuntu-k3s.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # ! This requires systemd installation in advance.
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.18.20+k3s1 sh -s - --write-kubeconfig-mode 644 --no-deploy traefik --docker
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.24.4+k3s1 sh -s - --write-kubeconfig-mode 644 --no-deploy traefik --docker
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
